99bcbab1ca553494666d9eac6ad40103b00bf31b
Ella Morano <ella@medgrocer.com>
2017-08-09 23:15:05 +0800

    generated controller for static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def gallery
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'static_pages/gallery'
+
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+  get 'static_pages/home'
+
+  root 'static_pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
